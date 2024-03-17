616b73d823ebb4eae0616037f2de1ff1ce55c031
Kdddru <dhk9817@gmail.com>
2024-03-17 23:06:04 +0900

    0317 pokemon class 정의

--- api/poke.js
+++ api/poke.js
@@ -1,28 +1,103 @@
 
-const getUrl = async (parm) =>{
-  const url = `https://pokeapi.co/api/v2/pokemon/${parm}`
+// const getUrl = async (param) =>{
+//   const url = `https://pokeapi.co/api/v2/pokemon/${param}`
 
-  const response = await fetch(url);
-  const json = await response.json();
+  // const response = await fetch(url);
+  // const json = await response.json();
 
-  const name_url = json.species.url
+  // const name_url = json.species.url
 
-  const types_url = json.types.map(( {type} )=> type.url);
+  // const types_url = json.types.map(( {type} )=> type.url);
 
-  return { name_url, types_url }
-}
+  // return { name_url, types_url }
+// }
 
-getUrl(1)
+// getUrl(1)
 
 
-const getName = async()=>{
-  const { name_url } = await getUrl(1);
+// const getName = async()=>{
+//   const { name_url } = await getUrl(1);
   
-  console.log(name_url);
+//   console.log(name_url);
+// }
+
+
+// getName();
+
+class Urls {
+  
+  constructor(param){
+    this.urls = this.getUrl(param);
+    
+  }
+  async getUrl(id){
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const response = await fetch(url);
+    const json = await response.json();
+  
+    const name_url = json.species.url
+  
+    const types_url = json.types.map(( {type} )=> type.url);
+  
+    
+
+    return  { name_url, types_url}
+  }
 }
 
 
-getName();
+class Pokemon extends Urls {
+
+  constructor(param){
+    //Urls class에게 
+    super(param)
+
+    this.name = this.getName();
+    this.type = this.getType();
 
 
+  }
 
+  async getName(){
+    const {name_url} =  await this.urls
+
+    const response = await fetch(name_url);
+    const json = await response.json();
+
+    const name = json.names.find(({language} )=> language.name === "ko" ).name
+    
+    return name
+  }
+
+  async getType(){
+    const {types_url} = await this.urls
+
+    const fetch_Data = types_url.map((url)=> fetch(url))
+
+    const type = await Promise.all(fetch_Data)
+    .then((response)=> Promise.all(response.map((res)=>res.json())))
+    .then((result)=>result.map(({names})=>{
+      const name = names.find(({language})=> language.name === "ko").name
+
+      return name
+    }))
+    
+    return type
+  }
+}
+
+
+async function a(){
+  const pokemon = new Pokemon(1);
+  
+
+  console.log(pokemon);
+
+  
+
+
+}
+
+
+a();
